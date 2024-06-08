438f310e3bcd00df1fbc067ed24fad1f0c67a8d9
ipitio <21136719+ipitio@users.noreply.github.com>
2024-06-08 07:28:57 -0400

    fix grep

--- update.sh
+++ update.sh
@@ -9,11 +9,10 @@ while IFS= read -r line; do
     owner=$(echo "$line" | cut -d'/' -f1)
     repo=$(echo "$line" | cut -d'/' -f2)
     image=$(echo "$line" | cut -d'/' -f3)
-    data=$(curl -sSLNZ https://github.com/"$owner"/"$repo"/pkgs/container/"$image")
     query='(?<=Total downloads</span>\n          <h3 title=")[^<]*'
-    pulls=$(grep -Pzo "$query" <<<"$data")
+    pulls=$(curl -sSLNZ https://github.com/"$owner"/"$repo"/pkgs/container/"$image" | grep -Pzo "$query")
     query="${query//\"/\"$pulls\">}"
-    pulls=$(grep -Pzo "$query" <<<"$data")
+    pulls=$(curl -sSLNZ https://github.com/"$owner"/"$repo"/pkgs/container/"$image" | grep -Pzo "$query")
 
     if [ -n "$pulls" ] && [ "$pulls" -eq "$pulls" ] 2>/dev/null && [ "$pulls" -gt 0 ]; then
         jq --arg owner "$owner" --arg repo "$repo" --arg image "$image" --arg pulls "$pulls" '
