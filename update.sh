cc6726d00381b4ed2a19c669cafba47c4139b4a8
ipitio <21136719+ipitio@users.noreply.github.com>
2024-06-10 14:10:54 -0400

    fix all %

--- update.sh
+++ update.sh
@@ -64,10 +64,10 @@ for i in $(jq -r '.[] | @base64' index.json); do
     my $repo = $ENV{"repo"};
     my $image = $ENV{"image"};
 
-    # replace any "%2F" with "%252F"
-    $owner =~ s/%2F/%252F/g;
-    $repo =~ s/%2F/%252F/g;
-    $image =~ s/%2F/%252F/g;
+    # replace any "%" with "%25"
+    $owner =~ s/%/%25/g;
+    $repo =~ s/%/%25/g;
+    $image =~ s/%/%25/g;
 
     s/\n\n(\[!\[.*)\n\n/\n\n$1 \[!\[$owner\/$repo\/$image\]\(https:\/\/img.shields.io\/badge\/dynamic\/json\?url=https%3A%2F%2Fraw.githubusercontent.com%2Fipitio%2Fghcr-pulls%2Fmaster%2Findex.json\&query=%24%5B%3F(%40.owner%3D%3D%22$owner%22%20%26%26%20%40.repo%3D%3D%22$repo%22%20%26%26%20%40.image%3D%3D%22$image%22)%5D.pulls\&label=$image\)\]\(https:\/\/github.com\/$owner\/$repo\/pkgs\/container\/$image\)\n\n/g;
 ' README.md > README.tmp && [ -f README.tmp ] && mv README.tmp README.md || :
