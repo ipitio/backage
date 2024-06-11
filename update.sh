fdf51bc325cd4a6df9d1ba88b29a15a818b3e86d
ipitio <21136719+ipitio@users.noreply.github.com>
2024-06-11 02:53:17 -0400

    add todo

--- update.sh
+++ update.sh
@@ -3,6 +3,8 @@
 # Usage: ./update.sh
 # Dependencies: curl, jq
 # Copyright (c) ipitio
+#
+# shellcheck disable=SC2015
 
 if ! command -v curl &>/dev/null || ! command -v jq &>/dev/null; then
     sudo apt-get update
@@ -16,21 +18,22 @@ done >pkg.tmp.txt
 mv pkg.tmp.txt pkg.txt
 [ -z "$(tail -c 1 pkg.txt)" ] || echo >>pkg.txt
 
-# setup templates
+# set up templates
 [ -f index.json ] || echo "[]" >index.json
 [ ! -f README.md ] || rm -f README.md
 \cp .README.md README.md
 
-# update the index with new counts if we get a response
+# update the index with new counts...
 while IFS= read -r line; do
     owner=$(echo "$line" | cut -d'/' -f1)
     repo=$(echo "$line" | cut -d'/' -f2)
     image=$(echo "$line" | cut -d'/' -f3)
-    html=$(curl -sSLN "https://github.com/$owner/$repo/pkgs/container/$image")
+    html=$(curl -sSLNZ "https://github.com/$owner/$repo/pkgs/container/$image")
     raw_pulls=$(echo -e "$html" | grep -Pzo '(?<=Total downloads</span>\n          <h3 title=")\d*')
     pulls=$(echo -e "$html" | grep -Pzo "(?<=Total downloads</span>\n          <h3 title=\"$raw_pulls\">)[^<]*")
     date=$(date -u +"%Y-%m-%d")
 
+    # ...if we get a response
     if [ -n "$pulls" ]; then
         jq --arg owner "$owner" --arg repo "$repo" --arg image "$image" --arg pulls "$pulls" --arg raw_pulls "$raw_pulls" --arg date "$date" '
             if . == [] then
@@ -43,11 +46,11 @@ while IFS= read -r line; do
     fi
 done <pkg.txt
 
-# sort the index by the number of raw_pulls and remove the latest date if it is the same as the previous
-jq 'sort_by(.raw_pulls | tonumber) | reverse | map(.raw_pulls_all |= with_entries(select(.key != keys[-1])))' index.json >index.tmp.json
+# sort the index by the number of raw_pulls greatest to smallest
+jq 'sort_by(.raw_pulls | tonumber) | reverse' index.json >index.tmp.json
 mv index.tmp.json index.json
 
-# update the README template with new badges
+# update the README template with badges...
 for i in $(jq -r '.[] | @base64' index.json); do
     _jq() {
         echo "$i" | base64 --decode | jq -r "$@"
@@ -59,22 +62,23 @@ for i in $(jq -r '.[] | @base64' index.json); do
     pulls=$(_jq '.pulls')
     raw_pulls=$(_jq '.raw_pulls')
     export owner repo image
+    printf "%s (%s) pulls \t\t\t | %s/%s/%s\n" "$pulls" "$raw_pulls" "$owner" "$repo" "$image"
 
+    # ...that have not been added yet
     grep -q "$owner/$repo/$image" README.md || perl -0777 -pe '
+    # map "%" to "%25"
     my $owner = $ENV{"owner"};
     my $repo = $ENV{"repo"};
     my $image = $ENV{"image"};
-    my $label = $ENV{"image"};
+    for ($owner, $repo, $image) {
+        s/%/%25/g;
+    }
 
-    # replace any "%" with "%25"
-    $owner =~ s/%/%25/g;
-    $repo =~ s/%/%25/g;
-    $image =~ s/%/%25/g;
-    # decode URL-encoded slashes for label
-    $label =~ s/%2F/\//g;
+    # map percent-encoded characters
+    my $label = $image;
+    $label =~ s/%([0-9A-Fa-f]{2})/chr(hex($1))/eg;
 
+    # add new badge
     s/\n\n(\[!\[.*)\n\n/\n\n$1 \[!\[$owner\/$repo\/$image\]\(https:\/\/img.shields.io\/badge\/dynamic\/json\?url=https%3A%2F%2Fraw.githubusercontent.com%2Fipitio%2Fghcr-pulls%2Fmaster%2Findex.json\&query=%24%5B%3F(%40.owner%3D%3D%22$owner%22%20%26%26%20%40.repo%3D%3D%22$repo%22%20%26%26%20%40.image%3D%3D%22$image%22)%5D.pulls\&label=$label\)\]\(https:\/\/github.com\/$owner\/$repo\/pkgs\/container\/$image\)\n\n/g;
 ' README.md > README.tmp && [ -f README.tmp ] && mv README.tmp README.md || :
-
-    printf "%s (%s) pulls \t\t\t | %s/%s/%s\n" "$pulls" "$raw_pulls" "$owner" "$repo" "$image"
 done
