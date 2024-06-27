31d41294b06a9d3b9dee6693e25da6569d675965
ipitio <21136719+ipitio@users.noreply.github.com>
2024-06-27 01:22:51 -0400

    this echo is fine

--- update.sh
+++ update.sh
@@ -56,7 +56,7 @@ sqlite3 "$INDEX_DB" "$table_pkg"
 rate_limit_start=$(date +%s)
 calls_to_api=0
 while IFS= read -r owner; do
-
+    echo "Processing $owner..."
     for owner_type in users orgs; do
         repos=""
         repos_page=0
