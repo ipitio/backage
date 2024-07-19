7bd1444320f0c899a38e60fe45ca65b908d5815d
ipitio <21136719+ipitio@users.noreply.github.com>
2024-07-19 14:04:28 -0400

    set -x

--- src/test/update.sh
+++ src/test/update.sh
@@ -6,5 +6,6 @@
 # shellcheck disable=SC1090,SC1091
 
 cd "${0%/*}"/.. || exit 1 && source lib.sh
+set -x
 update_owners "$@"
 [ "$(stat -c %s "$BKG_INDEX_SQL".zst)" -ge 1000 ] || exit 1 # db should not be empty
