72bd0bf4aeb6fc898cf22891735fdb9686d67645
ipitio <21136719+ipitio@users.noreply.github.com>
2024-07-19 04:52:26 -0400

    update readme

--- src/update.sh
+++ src/update.sh
@@ -9,15 +9,14 @@ source lib.sh
 
 main() {
     set_up
-    TODAY=$(get_BKG BKG_TODAY)
-    # remove owners from queue that have already been scraped in this batch
-    [ -n "$(get_BKG BKG_BATCH_FIRST_STARTED)" ] || set_BKG BKG_BATCH_FIRST_STARTED "$TODAY"
     set_BKG BKG_TIMEOUT "2"
+    TODAY=$(get_BKG BKG_TODAY)
+    [ -n "$(get_BKG BKG_BATCH_FIRST_STARTED)" ] || set_BKG BKG_BATCH_FIRST_STARTED "$TODAY"
     [[ "$1" != "0" ]] || get_BKG_set BKG_OWNERS_QUEUE | env_parallel --lb remove_owner
     [ -n "$(get_BKG BKG_OWNERS_QUEUE)" ] || set_BKG BKG_BATCH_FIRST_STARTED "$TODAY"
-    BKG_BATCH_FIRST_STARTED=$(get_BKG BKG_BATCH_FIRST_STARTED)
     [ -n "$(get_BKG BKG_RATE_LIMIT_START)" ] || set_BKG BKG_RATE_LIMIT_START "$(date -u +%s)"
     [ -n "$(get_BKG BKG_CALLS_TO_API)" ] || set_BKG BKG_CALLS_TO_API "0"
+    BKG_BATCH_FIRST_STARTED=$(get_BKG BKG_BATCH_FIRST_STARTED)
 
     # reset the rate limit if an hour has passed since the last run started
     if (($(get_BKG BKG_RATE_LIMIT_START) + 3600 <= $(date -u +%s))); then
@@ -35,8 +34,7 @@ main() {
     if [ "$1" = "0" ]; then
         [ -n "$(get_BKG BKG_LAST_SCANNED_ID)" ] || set_BKG BKG_LAST_SCANNED_ID "0"
         seq 1 10 | env_parallel --lb page_owner
-        query="select owner_id, owner from '$BKG_INDEX_TBL_PKG' where date not between date('$BKG_BATCH_FIRST_STARTED') and date('$TODAY') group by owner_id;"
-        sqlite3 "$BKG_INDEX_DB" "$query" | awk -F'|' '{print $1"/"$2}' | env_parallel --lb add_owner
+        sqlite3 "$BKG_INDEX_DB" "select owner_id, owner from '$BKG_INDEX_TBL_PKG' where date not between date('$BKG_BATCH_FIRST_STARTED') and date('$TODAY') group by owner_id;"" | awk -F'|' '{print $1"/"$2}'" | env_parallel --lb add_owner
     fi
 
     # add more owners
