5223ce6cf00a492d639339ddf632dd428de29842
ipitio <21136719+ipitio@users.noreply.github.com>
2024-07-20 05:54:26 -0400

    change test

--- src/test/refre.sh
+++ src/test/refre.sh
@@ -3,11 +3,14 @@
 # Usage: ./refre.sh
 # Copyright (c) ipitio
 #
-# shellcheck disable=SC1090,SC1091
+# shellcheck disable=SC1090,SC1091,SC2317
 
 cd "${0%/*}"/.. || exit 1 && source lib.sh
 refresh_owners "$@"
 
-for json in "$BKG_INDEX_DIR"/*/*/*.json; do
-    jq -e . <<<"$(cat "$json")" &>/dev/null || exit 1 # json should be valid
-done
+check_json() {
+    jq -e . <<<"$(cat "$1")" &>/dev/null || exit 1 # json should be valid
+}
+
+find index -type f -name '*.json' | env_parallel --halt now,fail=1 check_json
+exit $?
