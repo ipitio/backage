144bd095dd6db0426dcf54e379f85f4b5ca22c26
ipitio <21136719+ipitio@users.noreply.github.com>
2024-07-26 16:54:31 -0400

    echo

--- src/lib.sh
+++ src/lib.sh
@@ -46,7 +46,6 @@ get_BKG() {
     local res=""
 
     while ! ln "$BKG_ENV" "$BKG_ENV.lock" 2>/dev/null; do
-        echo "file: $BKG_ENV.lock"
         sleep 0.1
     done
 
@@ -62,6 +61,7 @@ set_BKG() {
     tmp_file=$(mktemp)
 
     while ! ln "$BKG_ENV" "$BKG_ENV.lock" 2>/dev/null; do
+        echo "file: $BKG_ENV.lock"
         sleep 0.1
     done
 
