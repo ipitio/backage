a7cd7e7c16ebc8c44ea270a1cb840ca3aabbb248
ipitio <21136719+ipitio@users.noreply.github.com>
2024-07-30 22:10:24 -0400

    fix changes

--- src/lib.sh
+++ src/lib.sh
@@ -567,7 +567,7 @@ refresh_package() {
     if [ "${version_count:--1}" -gt 0 ]; then
         version_newest_id=$(sqlite3 "$BKG_INDEX_DB" "select id from '$table_version_name' order by id desc limit 1;")
         rm -f "$json_file".*.json
-        run_parallel refresh_version "$(sqlite3 "$BKG_INDEX_DB" "select * from '$table_version_name' where date >= '$(get_BKG BKG_BATCH_FIRST_STARTED)' group by id;")"
+        run_parallel refresh_version "$(sqlite3 "$BKG_INDEX_DB" "select * from '$table_version_name' where date >= '$BKG_BATCH_FIRST_STARTED' group by id;")"
         cat "$json_file".*.json >>"$json_file"
         rm -f "$json_file".*.json
     else
@@ -722,7 +722,7 @@ refresh_owner() {
     echo "Refreshing $1..."
     [ -d "$BKG_INDEX_DIR" ] || mkdir "$BKG_INDEX_DIR"
     [ -d "$BKG_INDEX_DIR/$1" ] || mkdir "$BKG_INDEX_DIR/$1"
-    run_parallel refresh_package "$(sqlite3 "$BKG_INDEX_DB" "select * from '$BKG_INDEX_TBL_PKG' where owner='$1' and date >= '$(get_BKG BKG_BATCH_FIRST_STARTED)' group by package;")"
+    run_parallel refresh_package "$(sqlite3 "$BKG_INDEX_DB" "select * from '$BKG_INDEX_TBL_PKG' where owner='$1' and date >= '$BKG_BATCH_FIRST_STARTED' group by package;")"
     echo "Refreshed $1"
 }
 
@@ -733,6 +733,8 @@ set_up() {
     set_BKG BKG_TODAY "$(date -u +%Y-%m-%d)"
     set_BKG BKG_SCRIPT_START "$(date -u +%s)"
     set_BKG BKG_AUTO "${1:-0}"
+    TODAY=$(get_BKG BKG_TODAY)
+    BKG_BATCH_FIRST_STARTED=$(get_BKG BKG_BATCH_FIRST_STARTED)
 
     if [ ! -f "$BKG_INDEX_DB" ]; then
         command curl -sSLNZO "https://github.com/$GITHUB_OWNER/$GITHUB_REPO/releases/latest/download/$BKG_INDEX_SQL.zst"
@@ -810,8 +812,6 @@ update_owners() {
     set_up "$mode"
     set_BKG BKG_TIMEOUT "2"
     [ -n "$(get_BKG BKG_LAST_SCANNED_ID)" ] || set_BKG BKG_LAST_SCANNED_ID "0"
-    TODAY=$(get_BKG BKG_TODAY)
-    BKG_BATCH_FIRST_STARTED=$(get_BKG BKG_BATCH_FIRST_STARTED)
     local owners_to_update
     local rotated=false
     local query
@@ -941,6 +941,6 @@ update_owners() {
 
 refresh_owners() {
     set_up "$@"
-    sqlite3 "$BKG_INDEX_DB" "select distinct owner from '$BKG_INDEX_TBL_PKG';" | env_parallel --lb refresh_owner
+    sqlite3 "$BKG_INDEX_DB" "select distinct owner from '$BKG_INDEX_TBL_PKG' where date >= '$BKG_BATCH_FIRST_STARTED';" | env_parallel --lb refresh_owner
     clean_up
 }
