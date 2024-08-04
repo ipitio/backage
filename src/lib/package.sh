b686fff79ecef5cfad4c159f609a46b79a65b2b9
ipitio <21136719+ipitio@users.noreply.github.com>
2024-08-04 02:06:30 -0400

    move saving versions

--- src/lib/package.sh
+++ src/lib/package.sh
@@ -94,13 +94,14 @@ update_package() {
     [[ "$raw_downloads" =~ ^[0-9]+$ ]] || raw_downloads=-1
     table_version_name="${BKG_INDEX_TBL_VER}_${owner_type}_${package_type}_${owner}_${repo}_${package}"
 
-    if [ -n "$(sqlite3 "$BKG_INDEX_DB" "select name from sqlite_master where type='table' and name='$table_version_name';")" ]; then
-        run_parallel save_version "$(sqlite3 -json "$BKG_INDEX_DB" "select id, name, MAX(date), tags from '$table_version_name' group by id;" | jq -r '.[] | @base64')" || return $?
-    fi
-
     for page in $(seq 1 100); do
         local pages_left=0
         set_BKG BKG_VERSIONS_JSON_"${owner}_${package}" "[]"
+
+        if ((page == 1)) && [ -n "$(sqlite3 "$BKG_INDEX_DB" "select name from sqlite_master where type='table' and name='$table_version_name';")" ]; then
+            run_parallel save_version "$(sqlite3 -json "$BKG_INDEX_DB" "select id, name, MAX(date), tags from '$table_version_name' group by id;" | jq -r '.[] | @base64')" || return $?
+        fi
+
         page_version "$page"
         pages_left=$?
         ((pages_left != 3)) || return 3
