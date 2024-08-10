cc6822556752c37f4b8407f26577bea3bfaed567
ipitio <21136719+ipitio@users.noreply.github.com>
2024-08-09 23:49:25 -0400

    fix newest

--- src/lib/package.sh
+++ src/lib/package.sh
@@ -114,7 +114,7 @@ update_package() {
         jq -e . <<<"$versions_json" &>/dev/null || versions_json="[{\"id\":\"-1\",\"name\":\"latest\",\"tags\":\"\"}]"
         del_BKG BKG_VERSIONS_JSON_"${owner}_${package}"
         run_parallel update_version "$(jq -r '.[] | @base64' <<<"$versions_json")" || return $?
-        ((page != 1)) || version_newest_id=$(jq -r '.[] | select(.id) | .id' <<<"$versions_json" | sort -n | tail -n1)
+        ((page != 1)) || version_newest_id=$(jq -r '.[].id' <<<"$versions_json" | sort -n | tail -n1)
         ((pages_left != 2)) || break
     done
 
@@ -168,8 +168,7 @@ update_package() {
         \"raw_downloads_week\": $raw_downloads_week,
         \"raw_downloads_day\": $raw_downloads_day,
         \"version\": $(jq -c . "$BKG_INDEX_DIR/$owner/$repo/$package.d/"*.json | jq -s .)
-    }" | tr -d '\n' | jq -c . >"$json_file".tmp.json
-    jq -c --arg id "$version_newest_id" '.version |= map(if .id == ($id | tonumber) then .newest = true else . end)' "$json_file".tmp.json > "$json_file"
+    }" | tr -d '\n' | jq -c --arg id "$version_newest_id" '.version |= map(if .id == ($id | tonumber) then .newest = true else . end)' > "$json_file"
     sqlite3 "$BKG_INDEX_DB" "insert or replace into '$BKG_INDEX_TBL_PKG' (owner_id, owner_type, package_type, owner, repo, package, downloads, downloads_month, downloads_week, downloads_day, size, date) values ('$owner_id', '$owner_type', '$package_type', '$owner', '$repo', '$package', '$raw_downloads', '$raw_downloads_month', '$raw_downloads_week', '$raw_downloads_day', '$size', '$BKG_BATCH_FIRST_STARTED');"
     rm -f "${table_version_name}"_already_updated "${table_version_name}"_to_update all_"${table_version_name}"
     rm -f "$BKG_INDEX_DIR/$owner/$repo/$package.d/"*.json
