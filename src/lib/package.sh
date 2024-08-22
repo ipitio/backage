42bb08ab58a01e75347850f53c1553a0b909dc5e
ipitio <21136719+ipitio@users.noreply.github.com>
2024-08-22 17:10:33 -0400

    better check

--- src/lib/package.sh
+++ src/lib/package.sh
@@ -92,7 +92,7 @@ update_package() {
         tags text,
         primary key (id, date)
     );"
-    ! sqlite3 "$BKG_INDEX_DB" ".tables" | grep -q "^$old_table_version_name$" || sqlite3 "$BKG_INDEX_DB" "insert or replace into '$table_version_name' select * from '$old_table_version_name';"
+    [ -z "$(sqlite3 "$BKG_INDEX_DB" "select name from sqlite_master where type='table' and name='$old_table_version_name';")" ] || sqlite3 "$BKG_INDEX_DB" "insert or replace into '$table_version_name' select * from '$old_table_version_name';"
     sqlite3 "$BKG_INDEX_DB" "drop table if exists '$old_table_version_name';"
     if ! grep -q "^$owner_id|$owner|$repo|$package$" packages_already_updated; then
         html=$(curl "https://github.com/$owner/$repo/pkgs/$package_type/$package")
