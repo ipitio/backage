81b1a735b87f8f63e040838c16e8cf7e0aa0162b
ipitio <21136719+ipitio@users.noreply.github.com>
2024-08-28 05:13:34 -0400

    https://unix.stackexchange.com/a/603209

--- src/test/update.sh
+++ src/test/update.sh
@@ -5,24 +5,25 @@
 #
 # shellcheck disable=SC1090,SC1091
 
-check_json() {
-    [ -s "$1" ] || echo "Empty json: $1"
-    jq -e . <<<"$(cat "$1")" &>/dev/null || echo "Invalid json: $1"
-}
-
 if git ls-remote --exit-code origin index &>/dev/null; then
     git fetch origin index
     git worktree add index-branch index
     pushd index-branch || exit 1
     git pull
     popd || exit 1
-    \cp -r index-branch/* index/
-    git worktree remove index-branch
+    find . -type d -exec mkdir -vp ../index/{} \; -or mv -nv {} ../index/{} \;
+    git worktree remove -f index-branch
 fi
 
 pushd "${0%/*}/.." || exit 1
 source bkg.sh
 main "$@"
+
+check_json() {
+    [ -s "$1" ] || echo "Empty json: $1"
+    jq -e . <<<"$(cat "$1")" &>/dev/null || echo "Invalid json: $1"
+}
+
 # db should not be empty, error if it is
 [ "$(stat -c %s "$BKG_INDEX_SQL".zst)" -ge 1000 ] || exit 1
 # json should be valid, warn if it is not
@@ -34,9 +35,11 @@ git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git worktree add index-branch index
 pushd index-branch || exit 1
 git pull
-find ../index -type f -size +0c -exec cp --parents {} . \;
+pushd ../index || exit 1
+find . -type d -exec mkdir -vp ../index-branch/{} \; -or mv -nv {} ../index-branch/{} \;
+popd || exit 1
 git add .
 git commit -m "hydration"
-git push
+git push -f
 popd || exit 1
-git worktree remove index-branch
+git worktree remove -f index-branch
