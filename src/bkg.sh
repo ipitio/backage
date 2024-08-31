82cd2ec5bd055430876632f4b44a6101e49d20cb
ipitio <21136719+ipitio@users.noreply.github.com>
2024-08-31 07:54:35 -0400

    add color

--- src/bkg.sh
+++ src/bkg.sh
@@ -153,14 +153,13 @@ main() {
     \cp templates/.README.md "$BKG_ROOT"/README.md
     perl -0777 -pe 's/<GITHUB_OWNER>/'"$GITHUB_OWNER"'/g; s/<GITHUB_REPO>/'"$GITHUB_REPO"'/g; s/<GITHUB_BRANCH>/'"$GITHUB_BRANCH"'/g' "$BKG_ROOT"/README.md >README.tmp && [ -f README.tmp ] && mv README.tmp "$BKG_ROOT"/README.md || :
     sed -i '/^BKG_VERSIONS_.*=/d; /^BKG_PACKAGES_.*=/d; /^BKG_OWNERS_.*=/d; /^BKG_TIMEOUT=/d; /^BKG_SCRIPT_START=/d' "$BKG_ENV"
-    \cp "$BKG_ROOT"/README.md "$BKG_ROOT"/index/README.md
+    \cp "$BKG_ROOT"/README.md "$BKG_INDEX_DIR"/README.md
     # shellcheck disable=SC2016
-    sed -i 's/```prolog/```py/g; s/```jboss-cli/```js/g' "$BKG_ROOT"/README.md
-    sed -i 's/src\/img\/logo-b.png/logo-b.png/g' "$BKG_ROOT"/index/README.md
-    \cp img/logo-b.png "$BKG_ROOT"/index/logo-b.png
-    \cp img/logo.ico "$BKG_ROOT"/index/favicon.ico
-    \cp templates/.index.html "$BKG_ROOT"/index/index.html
-    sed -i 's/GITHUB_REPO/'"$GITHUB_REPO"'/g' "$BKG_ROOT"/index/index.html
+    sed -i 's/src\/img\/logo-b.png/logo-b.png/g; s/```py/```prolog/g; s/```js/```jboss-cli/g' "$BKG_INDEX_DIR"/README.md
+    \cp img/logo-b.png "$BKG_INDEX_DIR"/logo-b.png
+    \cp img/logo.ico "$BKG_INDEX_DIR"/favicon.ico
+    \cp templates/.index.html "$BKG_INDEX_DIR"/index.html
+    sed -i 's/GITHUB_REPO/'"$GITHUB_REPO"'/g' "$BKG_INDEX_DIR"/index.html
     rm -f packages_already_updated packages_all packages_to_update
     echo "Done!"
 }
