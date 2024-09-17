a5b432777e990b3014bc93a415f258ce9acb5f41
ipitio <21136719+ipitio@users.noreply.github.com>
2024-09-17 15:09:59 -0400

    search for users and orgs

--- src/lib/owner.sh
+++ src/lib/owner.sh
@@ -77,6 +77,31 @@ update_owner() {
     lower_owner=$(perl -pe 's/%([0-9A-Fa-f]{2})/chr(hex($1))/eg' <<<"${owner//%/%25}" | tr '[:upper:]' '[:lower:]')
     echo "Updating $owner..."
     [ -n "$(curl "https://github.com/orgs/$owner/people" | grep -zoP 'href="/orgs/'"$owner"'/people"' | tr -d '\0')" ] && export owner_type="orgs" || export owner_type="users"
+
+    if [ "$owner_type" = "users" ]; then
+        for page in $(seq 1 100); do
+            local user_orgs
+            local user_orgs_lines
+            user_orgs=$(query_api "$owner_type/$owner/orgs?per_page=100&page=$page")
+            (($? != 3)) || return 3
+            user_orgs_lines=$(jq -r '.[].login' <<<"$user_orgs" 2>/dev/null)
+            run_parallel request_owner "$user_orgs_lines"
+            (($? != 3)) || return 3
+            [ "$(wc -l <<<"$user_orgs_lines")" -eq 100 ] || break
+        done
+    else
+        for page in $(seq 1 100); do
+            local org_members
+            local org_members_lines
+            org_members=$(query_api "$owner_type/$owner/public_members?per_page=100&page=$page")
+            (($? != 3)) || return 3
+            org_members_lines=$(jq -r '.[].login' <<<"$org_members" 2>/dev/null)
+            run_parallel request_owner "$org_members_lines"
+            (($? != 3)) || return 3
+            [ "$(wc -l <<<"$org_members_lines")" -eq 100 ] || break
+        done
+    fi
+
     [ -d "$BKG_INDEX_DIR/$owner" ] || mkdir "$BKG_INDEX_DIR/$owner"
     set_BKG BKG_PACKAGES_"$owner" ""
     run_parallel save_package "$(sqlite3 "$BKG_INDEX_DB" "select package_type, package from '$BKG_INDEX_TBL_PKG' where owner_id = '$owner_id';" | awk -F'|' '{print "////"$1"//"$2}' | sort -uR)"
