cddc777ae514847a24420dc31251932aa7dd5263
ipitio <21136719+ipitio@users.noreply.github.com>
2024-09-27 19:04:20 -0400

    explore in the back

--- src/lib/package.sh
+++ src/lib/package.sh
@@ -63,11 +63,11 @@ update_package() {
     json_file="$BKG_INDEX_DIR/$owner/$repo/$package.json"
     table_version_name="${BKG_INDEX_TBL_VER}_${owner_type}_${package_type}_${owner}_${repo}_${package}"
 
-    if [ ! -f "${owner_id}_explored_${repo}" ]; then
+    (if [ ! -f "${owner_id}_explored_${repo}" ]; then
         run_parallel request_owner "$(comm -23 <(explore "$owner/$repo" | sort -u) <(awk -F'|' '{print $1"/"$2}' <packages_all | sort -u))"
         (($? != 3)) || return 3
         touch "${owner_id}_explored_${repo}"
-    fi
+    fi) &
 
     if grep -q "^$owner$" "$BKG_OPTOUT" || grep -q "^$owner/$repo$" "$BKG_OPTOUT" || grep -q "^$owner/$repo/$package$" "$BKG_OPTOUT"; then
         echo "$owner/$package was opted out!"
