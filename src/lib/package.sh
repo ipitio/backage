74dfb87d7848a3ba8241ad3f683ed008df6c3b90
ipitio <21136719+ipitio@users.noreply.github.com>
2024-09-27 22:49:07 -0400

    lock again

--- src/lib/package.sh
+++ src/lib/package.sh
@@ -62,12 +62,16 @@ update_package() {
     package=${package%/}
     json_file="$BKG_INDEX_DIR/$owner/$repo/$package.json"
     table_version_name="${BKG_INDEX_TBL_VER}_${owner_type}_${package_type}_${owner}_${repo}_${package}"
+    touch "${owner_id}_explored_${repo}"
+    while ! ln "${owner_id}_explored_${repo}" "${owner_id}_explored_${repo}.lock" 2>/dev/null; do :; done
 
-    if [ ! -f "${owner_id}_explored_${repo}" ]; then
-        touch "${owner_id}_explored_${repo}"
+    if ! grep -q . "${owner_id}_explored_${repo}"; then
+        echo . >"${owner_id}_explored_${repo}"
         run_parallel request_owner "$(comm -23 <(explore "$owner/$repo" | sort -u) <(awk -F'|' '{print $1"/"$2}' <packages_all | sort -u))"
         (($? != 3)) || return 3
     fi
+
+    rm -f "${owner_id}_explored_${repo}.lock"
 
     if grep -q "^$owner$" "$BKG_OPTOUT" || grep -q "^$owner/$repo$" "$BKG_OPTOUT" || grep -q "^$owner/$repo/$package$" "$BKG_OPTOUT"; then
         echo "$owner/$package was opted out!"
