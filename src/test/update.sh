4a987adbcfa8e1d220aa3a694b22f3a0b656b1de
ipitio <21136719+ipitio@users.noreply.github.com>
2024-10-18 13:59:15 -0400

    no if

--- src/test/update.sh
+++ src/test/update.sh
@@ -7,19 +7,16 @@
 
 pushd "$1" || exit 1
 
-if git ls-remote --exit-code origin index &>/dev/null; then
-    if [ -d index ]; then
-        [ ! -d index.bak ] || rm -rf index.bak
-        mv index index.bak
-    fi
-
-    git fetch origin index
-    git worktree add index index
-    pushd index || exit 1
-    git reset --hard origin/index
-    popd || exit 1
+if [ -d index ]; then
+    [ ! -d index.bak ] || rm -rf index.bak
+    mv index index.bak
 fi
 
+git fetch origin index
+git worktree add index index
+pushd index || exit 1
+git reset --hard origin/index
+popd || exit 1
 [ -f index/.env ] && \cp index/.env src/env.env || echo "No .env file found"
 pushd src || exit 1
 source bkg.sh
