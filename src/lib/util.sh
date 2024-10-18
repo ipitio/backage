d1352258373042bd057724861cfcb7235fdc37cd
ipitio <21136719+ipitio@users.noreply.github.com>
2024-10-18 05:49:42 -0400

    no sudo

--- src/lib/util.sh
+++ src/lib/util.sh
@@ -7,23 +7,23 @@
 # shellcheck disable=SC1090,SC1091,SC2015,SC2034
 
 apt_install() {
-    sudo apt-get install curl jq parallel sqlite3 sqlite3-pcre zstd libxml2-utils -yqq
+    apt-get install wget curl jq parallel sqlite3 sqlite3-pcre zstd libxml2-utils -yqq
 }
 
-if ! command -v curl &>/dev/null || ! command -v jq &>/dev/null || ! command -v sqlite3 &>/dev/null || ! command -v zstd &>/dev/null || ! command -v parallel &>/dev/null || ! command -v xmllint &>/dev/null || [ ! -f /usr/lib/sqlite3/pcre.so ]; then
+if ! command -v wget &>/dev/null || command -v curl &>/dev/null || ! command -v jq &>/dev/null || ! command -v sqlite3 &>/dev/null || ! command -v zstd &>/dev/null || ! command -v parallel &>/dev/null || ! command -v xmllint &>/dev/null || [ ! -f /usr/lib/sqlite3/pcre.so ]; then
     echo "Installing dependencies..."
     if ! apt_install; then
-        sudo apt-get update
+        apt-get update
         apt_install
     fi
 fi
 
 if ! yq -V | grep -q mikefarah; then
     echo "Installing yq..."
-    sudo rm -f /usr/bin/yq
+    rm -f /usr/bin/yq
     wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O yq
-    sudo mv yq /usr/bin/yq
-    sudo chmod +x /usr/bin/yq
+    mv yq /usr/bin/yq
+    chmod +x /usr/bin/yq
 fi
 
 echo "Dependencies verified!"
