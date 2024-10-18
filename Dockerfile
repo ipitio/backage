aa1ba8fd360cfa831e8eb12a6ff8c3c493dcab9e
ipitio <21136719+ipitio@users.noreply.github.com>
2024-10-18 05:33:37 -0400

    no setup

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM ubuntu:22.04
 WORKDIR /app
-COPY src/lib/setup.sh .
-RUN . setup.sh && verify_deps
+COPY src/lib/util.sh .
+RUN . util.sh
--- src/lib/util.sh
+++ src/lib/util.sh
@@ -6,8 +6,27 @@
 #
 # shellcheck disable=SC1090,SC1091,SC2015,SC2034
 
-source lib/setup.sh
-verify_deps
+apt_install() {
+    sudo apt-get install curl jq parallel sqlite3 sqlite3-pcre zstd libxml2-utils -yqq
+}
+
+if ! command -v curl &>/dev/null || ! command -v jq &>/dev/null || ! command -v sqlite3 &>/dev/null || ! command -v zstd &>/dev/null || ! command -v parallel &>/dev/null || ! command -v xmllint &>/dev/null || [ ! -f /usr/lib/sqlite3/pcre.so ]; then
+    echo "Installing dependencies..."
+    if ! apt_install; then
+        sudo apt-get update
+        apt_install
+    fi
+fi
+
+if ! yq -V | grep -q mikefarah; then
+    echo "Installing yq..."
+    sudo rm -f /usr/bin/yq
+    wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O yq
+    sudo mv yq /usr/bin/yq
+    sudo chmod +x /usr/bin/yq
+fi
+
+echo "Dependencies verified!"
 # shellcheck disable=SC2046
 source $(which env_parallel.bash)
 env_parallel --session
