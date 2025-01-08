3596c4ae2b1b4e2c2e5dab1783aaa13bcb7ca1df
ipitio <21136719+ipitio@users.noreply.github.com>
2025-01-08 17:48:15 -0500

    fix paths

--- src/lib/owner.sh
+++ src/lib/owner.sh
@@ -97,12 +97,13 @@ update_owner() {
     # merge packages into arrays
     find "$BKG_INDEX_DIR/$owner" -type f -name '*.json' | while read -r pkg; do
         for arr in "$owner" "$owner/$(jq -r '.repo' "$pkg")"; do
-            [ -f "$BKG_INDEX_DIR/$arr.json" ] || echo "[]" >"$BKG_INDEX_DIR/$arr.json"
-            jq -cs 'reduce .[0] as $obj (.[1]; . + [$obj]) | unique_by(.owner_id, .repo, .package)' "$pkg" "$BKG_INDEX_DIR/$arr.json" >"$BKG_INDEX_DIR/$arr.json.tmp"
-            jq -cs '{ ("object"): . }' "$BKG_INDEX_DIR/$arr.json.tmp" >"$BKG_INDEX_DIR/$arr.json"
-            ytox "$BKG_INDEX_DIR/$arr.json"
-            mv -f "$BKG_INDEX_DIR/$arr.json.tmp" "$BKG_INDEX_DIR/$arr.json"
+            [ -f "$BKG_INDEX_DIR/$arr.json" ] || echo "[]" >"$BKG_INDEX_DIR/$arr/.json"
+            jq -cs 'reduce .[0] as $obj (.[1]; . + [$obj]) | unique_by(.owner_id, .repo, .package)' "$pkg" "$BKG_INDEX_DIR/$arr/.json" >"$BKG_INDEX_DIR/$arr/.json.tmp"
+            jq -cs '{ ("object"): . }' "$BKG_INDEX_DIR/$arr/.json.tmp" >"$BKG_INDEX_DIR/$arr/.json"
+            ytox "$BKG_INDEX_DIR/$arr/.json"
+            mv -f "$BKG_INDEX_DIR/$arr/.json.tmp" "$BKG_INDEX_DIR/$arr.json"
             mv -f "$BKG_INDEX_DIR/$arr.json" "$BKG_INDEX_DIR/$arr/.json"
+            rm -f "$BKG_INDEX_DIR/$arr.xml" # temp
         done
     done
 
