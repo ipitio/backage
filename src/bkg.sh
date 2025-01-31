5e1da517a58d947cf77d3673c0a03c7999708d58
ipitio <21136719+ipitio@users.noreply.github.com>
2025-01-31 01:14:21 -0500

    better if

--- src/bkg.sh
+++ src/bkg.sh
@@ -62,8 +62,11 @@ main() {
         set_BKG BKG_MIN_CALLS_TO_API "0"
     fi
 
-    [ ! -f "$BKG_INDEX_DB" ] || mv "$BKG_INDEX_DB" "$BKG_INDEX_DB".bak
-    [ ! -f "$BKG_INDEX_SQL.zst" ] || unzstd -v -c "$BKG_INDEX_SQL.zst" | sqlite3 "$BKG_INDEX_DB"
+    if [ -f "$BKG_INDEX_SQL.zst" ]; then
+        [ ! -f "$BKG_INDEX_DB" ] || mv "$BKG_INDEX_DB" "$BKG_INDEX_DB".bak
+        unzstd -c "$BKG_INDEX_SQL.zst" | sqlite3 "$BKG_INDEX_DB"
+    fi
+
     [ -f "$BKG_INDEX_DB" ] || {
         [ -f "$BKG_INDEX_DB".bak ] && mv "$BKG_INDEX_DB".bak "$BKG_INDEX_DB" || sqlite3 "$BKG_INDEX_DB" ""
     }
