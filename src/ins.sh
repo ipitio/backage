c661d9e8da0c0248c588eb0b3b258650654a33b8
ipitio <ipitio@users.noreply.github.com>
2026-01-05 00:08:41 -0500

    use awk

--- src/ins.sh
+++ src/ins.sh
@@ -3,15 +3,22 @@
 base=$1
 inserts=$2
 
-[ -s "$base" ] || { cat "$inserts"; return; }
-[ -s "$inserts" ] || { cat "$base"; return; }
-awk 'BEGIN { srand() }
-	NR==FNR { if (!seen_base[$0]++) base[++n] = $0; next }
-	{ if (!seen_ins[$0]++) { slot = int(rand() * (n + 1)); ins[slot] = (ins[slot] ? ins[slot] ORS $0 : $0) } }
-	END {
-		for (i = 0; i <= n; i++) {
-			if (ins[i] != "") print ins[i]
-			if (i < n) print base[i + 1]
-		}
+awk '
+BEGIN { srand() }
+NR==FNR { base[++n] = $0; next }
+	{ ins[++m]  = $0 }
+END {
+	if (n == 0) { for (i = 1; i <= m; i++) print ins[i]; next }
+	if (m == 0) { for (i = 1; i <= n; i++) print base[i]; next }
+
+	for (i = 1; i <= m; i++) {
+	slot = int(rand() * (n + 1))
+	buckets[slot] = (slot in buckets ? buckets[slot] ORS ins[i] : ins[i])
 	}
+
+	for (i = 0; i <= n; i++) {
+	if (i in buckets) print buckets[i]
+	if (i < n) print base[i + 1]
+	}
+}
 ' "$base" "$inserts"
