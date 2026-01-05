7d71de65ecbba70130c9934fb3e6e106f2d7f593
ipitio <ipitio@users.noreply.github.com>
2026-01-05 01:34:05 -0500

    no next in end

--- src/ins.sh
+++ src/ins.sh
@@ -8,17 +8,20 @@ BEGIN { srand() }
 NR==FNR { base[++n] = $0; next }
 	{ ins[++m]  = $0 }
 END {
-	if (n == 0) { for (i = 1; i <= m; i++) print ins[i]; next }
-	if (m == 0) { for (i = 1; i <= n; i++) print base[i]; next }
+    if (n == 0) {
+        for (i = 1; i <= m; i++) print ins[i]
+    } else if (m == 0) {
+        for (i = 1; i <= n; i++) print base[i]
+    } else {
+        for (i = 1; i <= m; i++) {
+            slot = int(rand() * (n + 1))
+            buckets[slot] = (slot in buckets ? buckets[slot] ORS ins[i] : ins[i])
+        }
 
-	for (i = 1; i <= m; i++) {
-		slot = int(rand() * (n + 1))
-		buckets[slot] = (slot in buckets ? buckets[slot] ORS ins[i] : ins[i])
-	}
-
-	for (i = 0; i <= n; i++) {
-		if (i in buckets) print buckets[i]
-		if (i < n) print base[i + 1]
-	}
+        for (i = 0; i <= n; i++) {
+            if (i in buckets) print buckets[i]
+            if (i < n) print base[i + 1]
+        }
+    }
 }
 ' "$base" "$inserts"
